95c5c94887a0c0af2f9a5fe28ee7dca40814dd0d
aqmigliori <amiglior@calpoly.edu>
2026-09-18 13:34:00 -0700

    refactor ethernetHeader

--- trace.c
+++ trace.c
@@ -6,12 +6,43 @@
 #include <stdint.h>
 #include <string.h>
 
+// ARP 0x0806
+// IPv4 0x0800
 
-struct ExamplePkt {
+struct ethernetHeader {
     uint8_t dest[6];
     uint8_t src[6];
     uint16_t type;
 } __attribute__((packed)); //compiler removes all padding
+
+
+void ethernetPrint(char *packet, struct pcap_pkthdr header, int packetNumber) {
+    struct ethernetHeader *ex;
+
+    printf("\n");
+    printf("Packet number: %d", packetNumber);
+    ex = (struct ethernetHeader *) packet;
+
+    printf("  Packet Len: %d\n\n\t", header.caplen);
+
+    printf("Ethernet Header\n\t\t");
+
+    printf("Dest MAC: %x:%x:%x:%x:%x:%x\n\t\t", ex->dest[0], ex->dest[1], ex->dest[2],
+           ex->dest[3], ex->dest[4], ex->dest[5]);
+
+    printf("Source MAC: %x:%x:%x:%x:%x:%x\n\t\t", ex->src[0], ex->src[1], ex->src[2],
+           ex->src[3], ex->src[4], ex->src[5]);
+
+
+    printf("Type: ");
+    if (ntohs(ex->type) == 0x0806) {
+        printf("ARP\n");
+    } else if (ntohs(ex->type) == 0x0800) {
+        printf("IP\n");
+    } else {
+        printf("Unknown\n");
+    }
+}
 
 int main(int argc, char *argv[]) {
     int packetNumber = 1;
@@ -31,40 +62,13 @@ int main(int argc, char *argv[]) {
     struct pcap_pkthdr header;
     const u_char *packet = pcap_next(handle, &header);
 
-    struct ExamplePkt *ex;
-
     while (packet != NULL) {
-        printf("\n");
-        printf("Packet number: %d", packetNumber);
-        ex = (struct ExamplePkt *) packet;
-
-        printf("  Packet Len: %d\n\n\t", header.caplen);
-
-        printf("Ethernet Header\n\t\t");
-
-        printf("Dest MAC: %x:%x:%x:%x:%x:%x\n\t\t", ex->dest[0], ex->dest[1], ex->dest[2],
-            ex->dest[3], ex->dest[4], ex->dest[5]);
-
-        printf("Source MAC: %x:%x:%x:%x:%x:%x\n\t\t", ex->src[0], ex->src[1], ex->src[2],
-            ex->src[3], ex->src[4], ex->src[5]);
-
-
-        printf("Type: ");
-        if (ntohs(ex->type) == 0x0806) {
-            printf("ARP\n");
-        } else if (ntohs(ex->type) == 0x0800) {
-            printf("IP\n");
-        } else {
-            printf("Unknown\n");
-        }
-        // ARP 0x0806
-        // IPv4 0x0800
+        ethernetPrint((char*)packet, header, packetNumber);
         packetNumber++;
         packet = pcap_next(handle, &header); //increment packet
     }
 
 
     pcap_close(handle);
-
     return 0;
 }
